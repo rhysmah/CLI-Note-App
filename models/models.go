877ba1913422e247c7dfba71fbf3e2cf5aa46fe8
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-25 16:37:09 -0800

    feature: add basic Note struct and docs

---
+++ models/models.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+// Note represents a single note entry.
+// It contains simple data: a title, content, and tags.
+// It contains metadata: an identifer, creation, and modification timestamps.
+// The Note struct implements JSON serialization through struct tags.
+type Note struct {
+	ID         string    `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	Tags       []string  `json:"tags"`
+}
