bd87cee737351494facba23a7cb513996d86e99c
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-26 11:27:11 -0800

    feature: add initial new cobra command

--- cmd/new/new.go
+++ cmd/new/new.go
@@ -1,1 +1,17 @@
 package new
+
+import (
+	"github.com/rhysmah/CLI-Note-App/cmd/root"
+)
+
+const (
+	createCmdFull  = "create"
+	createCmdShort = "Create a new note"
+	createCmdDesc  = `Create a new note with the specified name.
+The note will be saved as '[note-name]_[date].txt' in your notes directory.
+Note names cannot contain special characters or exceed 50 characters.`
+)
+
+func init() {
+	root.RootCmd.AddCommand()
+}
