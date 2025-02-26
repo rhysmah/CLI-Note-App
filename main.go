e455805f75567bde4914ccdb953dfa5ae60986ed
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-25 17:18:29 -0800

    chore: update import

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/rhysmah/CLI-Note-App/cmd"
+import cmd "github.com/rhysmah/CLI-Note-App/cmd/root"
 
 func main() {
 	cmd.Execute()
