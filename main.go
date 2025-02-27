b871419bd37d417a19bc141235ec4bed158de931
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-27 12:39:44 -0800

    chore: add import

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	_ "github.com/rhysmah/CLI-Note-App/cmd/delete"
 	_ "github.com/rhysmah/CLI-Note-App/cmd/new"
 	"github.com/rhysmah/CLI-Note-App/cmd/root"
 )
