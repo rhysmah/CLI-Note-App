c1b2631b11e955c0af990284cf0217b3642b132a
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-02-27 10:44:46 -0800

    fix: update names, improve validation

--- cmd/new/validator.go
+++ cmd/new/validator.go
@@ -10,9 +10,10 @@ import (
 )
 
 const (
-	illegalChars      string = "\\/:*?\"<>|: ."
-	noteNameCharLimit int    = 50
-	dateTimeFormat    string = "2006_01_02_15_04"
+	illegalChars     string = "\\/:*?\"<>|: ."
+	noteNameMaxLimit int    = 50
+	noteNameMinLimit int    = 1
+	dateTimeFormat   string = "2006_01_02_15_04"
 )
 
 // newValidator creates and returns a new validator for Note objects
@@ -20,7 +21,8 @@ const (
 func newValidator() *validator.Validator[models.Note] {
 	return &validator.Validator[models.Note]{
 		Rules: []validator.ValidationRule[models.Note]{
-			validateNoteTitle,
+			validateNoteTitleLength,
+			validateNoteTitleIllegalCharacters,
 		},
 	}
 }
@@ -29,15 +31,16 @@ func newValidator() *validator.Validator[models.Note] {
 // - Does not exceed the character limit
 // - Does not contain illegal characters
 // Returns an error if validation fails.
-func validateNoteTitle(note models.Note) error {
+func validateNoteTitleLength(note models.Note) error {
 	noteNameTrimmed := strings.TrimSpace(note.Title)
 
-	if len(noteNameTrimmed) > noteNameCharLimit {
-		errMsg := fmt.Sprintf("name exceeds %d character limit", noteNameCharLimit)
+	if len(noteNameTrimmed) < noteNameMinLimit {
+		errMsg := fmt.Sprintf("note name %q must be greater than %d character", noteNameTrimmed, noteNameMinLimit)
 		return errors.New(errMsg)
 	}
-	if err := checkForIllegalCharacters(noteNameTrimmed); err != nil {
-		return fmt.Errorf("invalid characters in note name: %w", err)
+	if len(noteNameTrimmed) > noteNameMaxLimit {
+		errMsg := fmt.Sprintf("note name %q must be less than %d characters", noteNameTrimmed, noteNameMaxLimit)
+		return errors.New(errMsg)
 	}
 	return nil
 }
@@ -45,10 +48,10 @@ func validateNoteTitle(note models.Note) error {
 // checkForIllegalCharacters verifies that the note name doesn't contain any
 // forbidden characters defined in illegalChars. Returns an error listing any
 // illegal characters found.
-func checkForIllegalCharacters(noteName string) error {
+func validateNoteTitleIllegalCharacters(note models.Note) error {
 	var illegalCharsFound []rune
 
-	for _, char := range noteName {
+	for _, char := range note.Title {
 		if strings.ContainsRune(illegalChars, char) {
 			illegalCharsFound = append(illegalCharsFound, char)
 		}
