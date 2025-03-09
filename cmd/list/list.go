4db6a2e37b3ac170d2b2363c630329884fe934c7
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-03-09 14:48:30 -0700

    chore: add import, update command name, add init()

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -1,9 +1,9 @@
 package list
 
 import (
+	"github.com/rhysmah/CLI-Note-App/cmd/root"
 	"github.com/spf13/cobra"
 )
-
 
 const (
 	listCmdFull  = "list"
@@ -11,16 +11,21 @@ const (
 	listCmdDesc  = "Display a list of all your notes."
 )
 
+func init() {
+	listCmd := ListCommand()
+	root.RootCmd.AddCommand(listCmd)
+}
+
 // NewCommand creates and returns a cobra.Command for creating new notes.
 // The command requires exactly one argument: the note title.
-func NewCommand() *cobra.Command {
+func ListCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   listCmdFull,
 		Short: listCmdShort,
 		Long:  listCmdDesc,
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-		
+
 			return nil
 		},
 	}
