761c834a9d0db3be713177a55527ee1aea75ba99
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-03-13 20:33:57 -0700

    feature: add sorting and reverse functions

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -1,8 +1,15 @@
 package list
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/rhysmah/CLI-Note-App/cmd/root"
+	"github.com/rhysmah/CLI-Note-App/db"
+	"github.com/rhysmah/CLI-Note-App/models"
 	"github.com/spf13/cobra"
+
+	bolt "go.etcd.io/bbolt"
 )
 
 const (
@@ -14,8 +21,6 @@ const (
 func init() {
 	listCmd := ListCommand()
 	root.RootCmd.AddCommand(listCmd)
-
-	
 }
 
 // NewCommand creates and returns a cobra.Command for creating new notes.
@@ -25,14 +30,28 @@ func ListCommand() *cobra.Command {
 		Use:   listCmdFull,
 		Short: listCmdShort,
 		Long:  listCmdDesc,
-		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			// Get flags
+			sort, _ := cmd.Flags().GetString("sort-by")
+			reverse, _ := cmd.Flags().GetBool("reverse")
 
 			// Convert flag values to SortType, OrderType
+			sortBy := convertToSortBy(sort)
+			orderBy := convertToSortOrder(reverse)
 
 			// Sort and displays values based on flags
+
+			notes, err := getNotes(root.NotesDB)
+			if err != nil {
+				return fmt.Errorf("error opening database")
+			}
+
+			sortNotes(notes, sortBy, orderBy)
+
+			for _, note := range notes {
+				fmt.Println(note.Title)
+			}
 
 			return nil
 		},
@@ -43,4 +62,56 @@ func ListCommand() *cobra.Command {
 	cmd.Flags().BoolP("reverse", "r", false, "Reverse the sort order")
 
 	return cmd
+}
+
+func convertToSortBy(sort string) SortBy {
+	switch sort {
+	case "modified":
+		return SortByModified
+	case "created":
+		return SortByCreated
+	case "title":
+		return SortByTitle
+	default:
+		return SortByTitle
+	}
+}
+
+func convertToSortOrder(reverse bool) SortOrder {
+	switch reverse {
+	case false:
+		return SortOrderAscending
+	case true:
+		return SortOrderDescending
+	default:
+		return SortOrderAscending
+	}
+}
+
+func getNotes(database *bolt.DB) ([]models.Note, error) {
+	var notes []models.Note
+
+	err := database.View(func(tx *bolt.Tx) error {
+		
+		notesBucket := tx.Bucket([]byte(db.NotesBucket))
+		if notesBucket == nil {
+			return fmt.Errorf("bucket %s does not exist", db.NotesBucket)
+		}
+
+		return notesBucket.ForEach(func(k, v []byte) error {
+			var note models.Note
+			if err := json.Unmarshal(v, &note); err != nil {
+				return fmt.Errorf("error reading note data: %w", err)
+			}
+
+			notes = append(notes, note)
+			return nil
+		})
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving note titles: %w", err)
+	}
+
+	return notes, nil
 }
