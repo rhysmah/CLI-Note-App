6c587db4a964ebdf925e3d61d7d8b24b1496e548
Rhys Mahannah <rhys.mahannah@gmail.com>
2025-04-05 09:48:43 -0700

    feature: add app text

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -15,14 +15,33 @@ var (
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "CLI-Note-App",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "cli-note",
+	Short: "A simple CLI-based note-taking app",
+	Long: `CLI Note is exactly as the name implies: a dead simple CLI-based note-taking app.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+You can create, edit, delete, and list your notes.
+
+Basic Commands:
+	new         Create a new .txt file
+	edit        Open a file using your OS's default text editor
+	delete      Delete a file via filename
+	list        List all notes (name, creation date, modified date)
+
+When you run CLI Notes for the first time, a small database is created locally on your machine.
+This database is located in your home directory at ~/.cli-notes/
+
+Examples:
+  # Create a new note
+  cli-note new "Shopping List"
+  
+  # Edit an existing note
+  cli-note edit "Shopping List"
+  
+  # Delete a note
+  cli-note delete "Shopping List"
+  
+  # List all your notes
+  cli-note list`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		var err error
 
